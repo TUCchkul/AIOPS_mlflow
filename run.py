6c29c34272973c17129c901121130e281d5b1f9b
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-05-16 09:37:52 +0200

    add run

---
+++ run.py
@@ -0,0 +1,7 @@
+import os      
+p1s=range(0,5)
+p2s=range(0,5)
+for p1 in p1s:
+    for p2 in p2s:
+        print(f"logging experiments for p1:{p1}, p2: {p2}")
+        os.system(f"python demo.py -p1 {p1} -p2 {p2}")
